1a6197226c4edf2094eaf5c97f1820c9e9a8c3d9
norahwinda <norahwinda48@gmail.com>
2023-11-07 12:07:59 +0300

    testing repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Jokes from './Jokes.js'
+import JokesData from "./JokesData.js";
 
 function App() {
+  const jokeElement = JokesData.map(joke =>{
+    return <Jokes setup = {joke.setup} punchline = {joke.punchline} />
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {jokeElement}
     </div>
   );
 }
---
+++ src/JokesData.js
@@ -0,0 +1,22 @@
+export default[
+    {
+        setup:"I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face lit when she opens it"
+    },
+    {
+        setup:"How did the hacker escape the police",
+        punchline: "He just Ransomware"
+    },
+    {
+        setup:"Why don't pirates travel on mountain roads?",
+        punchline: "Scurvy"
+    },
+    {
+        setup: "Why do bees stay in the hive in the winter",
+        punchline: "Swarm"
+    },
+    {
+        setup:"What's the best thing about Switzerland",
+        punchline: "I don't know, but the flag is a big plus!"
+    }
+]
