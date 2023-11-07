a417ad072f2594078ffdf4b481990c5585f30c72
norahwinda <norahwinda48@gmail.com>
2023-11-07 12:20:47 +0300

    first commit

--- src/JokesData.js
+++ src/JokesData.js
@@ -1,7 +1,7 @@
 export default[
     {
         setup:"I got my daughter a fridge for her birthday.",
-        punchline: "I can't wait to see her face lit when she opens it"
+        punchline: "I can't wait Hello see her face lit when she opens it"
     },
     {
         setup:"How did the hacker escape the police",
